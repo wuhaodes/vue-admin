f4fca13e1a883b2c1af63f0024db3df97f05cef4
wuhaodes <wuhaodes@qq.com>
2018-10-23 23:02:51 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
     baseUrl: '/', // 根域上下文目录
-    outputDir: 'dist', // 构建输出目录
+    outputDir: 'docs', // 构建输出目录
     assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
     lintOnSave: false, // 是否开启eslint保存检测，有效值：ture | false | 'error'
     runtimeCompiler: true, // 运行时版本是否需要编译
